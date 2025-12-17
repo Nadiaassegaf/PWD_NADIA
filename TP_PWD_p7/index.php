c5ce1a3fb92231a0e42e6d697ad1807953eaf110
Nadiaassegaf <nadia.alisia.NA@gmail.com>
2025-12-17 18:06:04 +0700

    Add files via upload
    ini perbaikannya

--- TP_PWD_p7/index.php
+++ TP_PWD_p7/index.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Tabel Perkalian</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    <?php
-    echo "Menampilkan Bilangan Genap <br>";
-    for ($i = 1; $i <= 10; $i++) {
-        if ($i % 2 == 0) {
-            echo "$i ";
-        }
-    }
-    ?>
 
-    <br><br>
-    <table border="1px solid">
-        <tr>
-            <th>Bilangan</th>
-            <?php
-            for ($i = 1; $i <= 10; $i++) {
-                echo "<th style='background-color:#00cc44'>$i</th>";
-            }
-            ?>
-        </tr>
+<?php
+echo "<h3>Menampilkan Bilangan Genap</h3>";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . " ";
+    }
+}
+?>
+
+<br><br>
+
+<table>
+    <tr>
+        <th class="header">Bilangan</th>
         <?php
         for ($i = 1; $i <= 10; $i++) {
-            echo "<tr>";
-            echo "<th  style='background-color:#00cc44'>$i</th>";
-            for ($j = 1; $j <= 10; $j++) {
-                $hasil = $i * $j;
-                if ($hasil%2==0) {
-                    echo "<td class='diagonal'  style='background-color: cyan'>$hasil</td>";
-                } else {
-                    echo "<td  style='background-color:yellow'>$hasil</td>";
-                }
-            }
-            echo "</tr>";
+            echo "<th class='header'>$i</th>";
         }
         ?>
-    </table>
-</body>
+    </tr>
 
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<tr>";
+        echo "<th class='header'>$i</th>";
+
+        for ($j = 1; $j <= 10; $j++) {
+            $hasil = $i * $j;
+
+            if ($hasil % 2 == 0) {
+                echo "<td class='genap'>$hasil</td>";
+            } else {
+                echo "<td class='ganjil'>$hasil</td>";
+            }
+        }
+
+        echo "</tr>";
+    }
+    ?>
+</table>
+
+</body>
 </html>
